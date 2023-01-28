03954325ba7f6ce73b705ca10755a462c5f7c02a
Smudex <99404643+Smudex@users.noreply.github.com>
2023-01-28 16:52:06 +0100

    Add files via upload

---
+++ ExpirEats/ExpirEats/ExpirEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpirEatsApp.swift
+//  ExpirEats
+//
+//  Created by Sander Hafstad on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpirEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
